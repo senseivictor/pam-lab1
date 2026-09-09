c77099d2ad2da0cd4ae7cebaad1d128139841391
victor pavalache <victorpavalache2004@gmail.com>
2026-09-09 18:10:09 +0300

    changes to test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,12 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:conversie_moneda/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Currency converter title smoke test', (WidgetTester tester) async {
+    // 1. Build our app (Using CurrencyConverterApp instead of MyApp)
+    await tester.pumpWidget(const CurrencyConverterApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // 2. Verify that the title "Conversie monedă" is found
+    expect(find.text('Conversie monedă'), findsOneWidget);
   });
 }
